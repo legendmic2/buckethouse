619a711d250990cadb66c3c7289b5f1ad8b72f45
legendmic2 <goonoo0731@gmail.com>
2022-01-11 16:56:31 +0900

    [New Feature] MUI 적용, Page Layout 구현, HashRouter 적용
    description:
    - yarn package 內 MUI 적용
    - - MUI Template을 이용하여 Page Layout 구현
    - BrowserRouter => HashRouter 변경
    - - RouteMain 內 Redirect 적용
    - - BackEnd에서 "/" GET Request 받는 경우에 대한 exception 처리 구현
     - - - IndexController.java

--- src/main/react_ts/src/routes/RouteMain.tsx
+++ src/main/react_ts/src/routes/RouteMain.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import AppContext from "../app-context";
 import HomePage from "../pages/Home";
 import RootStore from "../stores/RootStore";
 import RootApi from "../apis/RootApi";
+import Blog from "../pages/blogTest/Blog";
 
 const store = new RootStore();
 const api = new RootApi(store);
@@ -12,7 +13,9 @@ const RouteMain = () => {
     return (
         <AppContext.Provider value={{store, api}}>
             <Routes>
-                <Route path="/*" element={<HomePage/>}/>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/blog" element={<Blog/>}/>
+                <Route path="*" element={<Navigate replace to="/"/>}/>
             </Routes>
         </AppContext.Provider>
     );
